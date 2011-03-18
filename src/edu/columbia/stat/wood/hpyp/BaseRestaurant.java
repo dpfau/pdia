dde34c39c6c41363273f3a7d0dccf5a2a9f44d11
David Pfau <hotmail.com@gmail.com>
2011-03-18 15:17:39 -0400

    Committed hpyp package

---
+++ src/edu/columbia/stat/wood/hpyp/BaseRestaurant.java
@@ -0,0 +1,113 @@
+package edu.columbia.stat.wood.hpyp;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.TreeMap;
+
+public class BaseRestaurant extends Restaurant implements Serializable {
+
+    public int alphabetSize; // If -1, this is a geometric distribution, if positive, uniform distribution
+    public int customers;
+    public double lambda;
+    private static final long serialVersionUID = 1L;
+
+    public BaseRestaurant(int alphabetSize) {
+        this.alphabetSize = alphabetSize;
+        customers = 0;
+    }
+
+    public BaseRestaurant() {
+        customers = 0;
+        alphabetSize = -1;
+        lambda = 0.001;
+    }
+
+    public void set(int cust) {
+        customers = cust;
+    }
+
+    @Override
+    public double predictiveProbability(int type) {
+        if (alphabetSize > -1) {
+            return 1.0 / alphabetSize;
+        }
+        return Math.exp(-1.0 * type * lambda) - Math.exp(-1.0 * (type + 1) * lambda);
+    }
+
+    @Override
+    public int[] seat(int type) {
+        customers += 1;
+        return null;
+    }
+
+    @Override
+    public void seatKN(int type) {
+        customers += 1;
+    }
+
+    @Override
+    public void unseat(int type) {
+        if (alphabetSize > -1) {
+            customers -= 1;
+        }
+        assert (customers > -1);
+    }
+
+    @Override
+    public double logLik() {
+        if (alphabetSize > -1) {
+            return customers * Math.log(1.0 / alphabetSize);
+        }
+        return 0.0;
+    }
+
+    @Override
+    public void predictiveProbability(double[] p) {
+        if (alphabetSize > -1) {
+            assert (p.length == alphabetSize);
+            Arrays.fill(p, 1.0D / alphabetSize);
+        } else {
+            throw new RuntimeException("not supported in the case of infinite base distribution");
+        }
+    }
+
+    @Override
+    public int generate() {
+        double r = RNG.nextDouble();
+
+        int ind = -1;
+        double cuSum = 0.0;
+        while (cuSum <= r) {
+            ind++;
+            if (this.alphabetSize > -1) {
+                cuSum += 1.0D / alphabetSize;
+                continue;
+            }
+            cuSum += Math.exp(-1.0 * ind * lambda) - Math.exp(-1.0D * (ind + 1) * lambda);
+        }
+
+        return ind;
+    }
+
+    public int generateNewType(TreeMap<Integer, int[]> tblMap) {
+        assert (this.alphabetSize == -1);
+
+        double totalWeight = 1.0;
+        for (Integer type : tblMap.keySet()) {
+            totalWeight -= Math.exp(-1.0 * type * lambda) - Math.exp(-1.0D * (type + 1) * lambda);
+        }
+
+        double r = RNG.nextDouble();
+        int newType = -1;
+        double cuSum = 0.0D;
+        while (cuSum <= r) {
+            newType++;
+            if (tblMap.get(newType) == null) {
+                double pp = predictiveProbability(newType);
+                cuSum += pp / totalWeight;
+            }
+        }
+
+        return newType;
+    }
+}
---
+++ src/edu/columbia/stat/wood/hpyp/RestaurantFranchise.java
@@ -0,0 +1,307 @@
+package edu.columbia.stat.wood.hpyp;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class RestaurantFranchise implements Serializable {
+
+    public Restaurant root;
+    public Discounts discounts;
+    public Concentrations concentrations;
+    private int depth;
+    private int alphabetSize;
+    private int[] currentContext;
+    private static final long serialVersionUID = 1L;
+    private double[] dLogLik;
+    private double[] cLogLik;
+    private int num = 0;
+
+    public RestaurantFranchise(int depth, int alphabetSize) {
+        this.alphabetSize = alphabetSize;
+        this.depth = depth;
+
+        discounts = new Discounts(new double[]{0.0});
+        concentrations = new Concentrations(new double[]{10.0, 10.0});
+
+        root = new Restaurant(new BaseRestaurant(alphabetSize), discounts.get(0), concentrations.get(0));
+    }
+
+    public RestaurantFranchise(int depth) {
+        this.depth = depth;
+
+        discounts = new Discounts(new double[]{0.5, 0.5});
+        concentrations = new Concentrations(new double[]{20.0, 15.0});
+
+        root = new Restaurant(new BaseRestaurant(), discounts.get(0), concentrations.get(0));
+        alphabetSize = -1;
+    }
+
+    public double continueSequence(int type) {
+        double p = seat(type, currentContext);
+        updateContext(type);
+        return Math.log(p);
+    }
+
+    public int depth() {
+        return depth;
+    }
+
+    public double seat(int type, int[] context) {
+        Restaurant r = get(context);
+        double p = r.predictiveProbability(type);
+        r.seat(type);
+
+        return p;
+    }
+
+    public int generate(int[] context) {
+        return get(context).generate();
+    }
+
+    public void unseat(int type, int[] context) {
+        get(context).unseat(type);
+    }
+
+    public double predictiveProbability(int type, int[] context) {
+        return getDontAdd(context).predictiveProbability(type);
+    }
+
+    public HashMap<Integer, MutableDouble> predictiveProbabilityExisingTypes(int[] context) {
+        HashMap returnMap = new HashMap(root.tableMap.size() + 1);
+        Restaurant r = getDontAdd(context);
+        double cuSum = 0.0;
+
+        for (Integer type : root.tableMap.keySet()) {
+            double p = r.predictiveProbability(type);
+            cuSum += p;
+            returnMap.put(type, new MutableDouble(p));
+        }
+
+        returnMap.put(-1, new MutableDouble(1.0 - cuSum));
+
+        return returnMap;
+    }
+
+    public int generateNewType() {
+        BaseRestaurant r = (BaseRestaurant) root.getParent();
+        return r.generateNewType(root.tableMap);
+    }
+
+    public double[] predictiveProbability(int[] context) {
+        double[] p = new double[alphabetSize];
+        get(context).predictiveProbability(p);
+        return p;
+    }
+
+    public void sample() {
+        sampleSeating(root);
+        sampleDiscounts(0.07);
+        sampleConcentrations(1.5);
+    }
+
+    private void sampleSeating(Restaurant r) {
+        for (Restaurant child : r.values()) {
+            sampleSeating(child);
+        }
+
+        HashSet<Integer> toRemove = new HashSet();
+
+        for (Integer key : r.keySet()) {
+            Restaurant c = (Restaurant) r.get(key);
+            if ((c.isEmpty()) && (c.tables() == 0)) {
+                toRemove.add(key);
+            }
+        }
+
+        for (Integer key : toRemove) {
+            r.remove(key);
+        }
+
+        r.sampleSeatingArrangements();
+    }
+
+    public void sampleDiscounts(double proposalSTD) {
+        double[] currentDiscounts = new double[discounts.length()];
+
+        logLik();
+
+        double[] currentLogLik = new double[discounts.length()];
+        System.arraycopy(dLogLik, 0, currentLogLik, 0, discounts.length());
+
+        for (int i = 0; i < discounts.length(); i++) {
+            MutableDouble d = discounts.get(i);
+            currentDiscounts[i] = d.doubleVal();
+            double proposal = currentDiscounts[i] + Restaurant.RNG.nextGaussian() * proposalSTD;
+            proposal = (proposal >= 1.0) || (proposal <= 0.0) ? currentDiscounts[i] : proposal;
+            d.set(proposal);
+        }
+
+        logLik();
+
+        for (int i = 0; i < discounts.length(); i++) {
+            if (Restaurant.RNG.nextDouble() >= Math.exp(dLogLik[i] - currentLogLik[i])) {
+                discounts.get(i).set(currentDiscounts[i]);
+            }
+        }
+    }
+
+    public void sampleConcentrations(double proposalSTD) {
+        double[] currentConcentrations = new double[concentrations.length()];
+
+        logLik();
+
+        double[] currentLogLik = new double[concentrations.length()];
+        System.arraycopy(cLogLik, 0, currentLogLik, 0, concentrations.length());
+
+        for (int i = 0; i < concentrations.length(); i++) {
+            MutableDouble c = concentrations.get(i);
+            currentConcentrations[i] = c.doubleVal();
+            double proposal = currentConcentrations[i] + Restaurant.RNG.nextGaussian() * proposalSTD;
+            proposal = proposal <= 0.0 ? currentConcentrations[i] : proposal;
+            c.set(proposal);
+        }
+
+        logLik();
+
+        for (int i = 0; i < concentrations.length(); i++) {
+            double r = Math.exp(cLogLik[i] - concentrations.get(i).doubleVal() - currentLogLik[i] + currentConcentrations[i]);
+            if (Restaurant.RNG.nextDouble() >= r) {
+                concentrations.get(i).set(currentConcentrations[i]);
+            }
+        }
+    }
+
+    public double logLik() {
+        double logLik = root.getParent().logLik();
+        dLogLik = new double[discounts.length()];
+        cLogLik = new double[concentrations.length()];
+
+        logLik(root, 0);
+        for (double d : dLogLik) {
+            logLik += d;
+        }
+
+        return logLik;
+    }
+
+    public void logLik(Restaurant r, int d) {
+        int dIndex = d < discounts.length()      ? d : discounts.length()      - 1;
+        int cIndex = d < concentrations.length() ? d : concentrations.length() - 1;
+
+        double logLik = r.logLik();
+
+        dLogLik[dIndex] += logLik;
+        cLogLik[cIndex] += logLik;
+
+        for (Restaurant child : r.values()) {
+            logLik(child, d + 1);
+        }
+    }
+
+    public Restaurant getDontAdd(int[] context) {
+        if ((context == null) || (context.length == 0)) {
+            return root;
+        }
+
+        int ci = context.length - 1;
+        int d = 0;
+        Restaurant current = root;
+        Restaurant child = null;
+
+        while ((d < depth) && (ci > -1)) {
+            child = current.get(context[ci]);
+            if (child == null) {
+                break;
+            }
+            current = child;
+            ci--;
+            d++;
+        }
+
+        return current;
+    }
+
+    public Restaurant get(int[] context) {
+        if ((context == null) || (context.length == 0)) {
+            return root;
+        }
+
+        int ci = context.length - 1;
+        int d = 0;
+        Restaurant current = root;
+        Restaurant child = null;
+
+        while ((d < depth) && (ci > -1)) {
+            child = current.get(context[ci]);
+            if (child == null) {
+                child = new Restaurant(current, discounts.get(d + 1), concentrations.get(d + 1));
+                current.put(context[ci], child);
+            }
+            current = child;
+            ci--;
+            d++;
+        }
+
+        return current;
+    }
+
+    private void updateContext(int obs) {
+        if (depth != 0) {
+            if (currentContext == null) {
+                currentContext = new int[]{obs};
+            } else if (currentContext.length < depth) {
+                int[] newContext = new int[currentContext.length + 1];
+                System.arraycopy(currentContext, 0, newContext, 0, currentContext.length);
+                newContext[currentContext.length] = obs;
+                currentContext = newContext;
+            } else {
+                for (int i = 0; i < depth - 1; i++) {
+                    currentContext[i] = currentContext[(i + 1)];
+                }
+                currentContext[(depth - 1)] = obs;
+            }
+        }
+    }
+
+    public void checkConsistency() {
+        checkConsistency(root);
+    }
+
+    public void checkConsistency(Restaurant r) {
+        if (!r.isEmpty()) {
+            int childrenTables = 0;
+
+            for (Restaurant child : r.values()) {
+                childrenTables += child.tables();
+                checkConsistency(child);
+            }
+
+            if (r.customers != childrenTables) {
+                System.out.println(num++);
+            }
+        }
+    }
+
+    public void print() {
+        print(root, new int[0]);
+    }
+
+    public void print(Restaurant r, int[] rKey) {
+        for (Integer k : r.keySet()) {
+            int[] cKey = new int[rKey.length + 1];
+            System.arraycopy(rKey, 0, cKey, 1, rKey.length);
+            cKey[0] = k.intValue();
+            print(r.get(k), cKey);
+        }
+
+        System.out.print("[");
+        for (int keyVar : rKey) {
+            System.out.print(keyVar + " ");
+        }
+        System.out.println("]");
+
+        r.printTableMap();
+        System.out.println();
+    }
+}
