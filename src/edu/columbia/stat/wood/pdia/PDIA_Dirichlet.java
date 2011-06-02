cdaa70ace006ac96b45a5ff6ab5006165c8b2192
David Pfau <hotmail.com@gmail.com>
2011-06-02 05:14:53 -0400

    Checking accept rate of MH sampler

--- src/edu/columbia/stat/wood/pdia/PDIA_Dirichlet.java
+++ src/edu/columbia/stat/wood/pdia/PDIA_Dirichlet.java
@@ -264,10 +264,12 @@ public class PDIA_Dirichlet implements Serializable, PDIA {
         if (Math.log(RNG.nextDouble()) < pLogLik - cLogLik) { // accept
             rf.unseat(currentType, context);
             rf.seat(proposedType, context);
+            System.out.println("accept");
         } else { // reject
             cMatrix = oldCounts;
             logLike = cLogLik;
             dMatrix.put(p, currentType);
+            System.out.println("reject");
         }
     }
 
