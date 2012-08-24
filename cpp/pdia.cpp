306bd2c4186f43bbe822d9a2e6c1fa044a5e76cf
David Pfau <hotmail.com@gmail.com>
2012-08-24 18:45:37 -0400

    Changed sampling so that it is something done to a datum object, rather than returning a new value.  This makes it easier to ekstend to the case of CRPs, where sampling binds a sampled value to a table in the CRP object.

--- cpp/pdia.cpp
+++ cpp/pdia.cpp
@@ -13,9 +13,16 @@ int discreteSample(double * cdf, int size) {
 }
 
 template <class T>
+class Datum {
+	public:
+		T * value;
+		Datum() { value = NULL; }
+};
+
+template <class T>
 class Distribution {
 	public:
-		T sample();
+		void sample(Datum<T> * d);
 		double score(T t);
 };
 
@@ -24,14 +31,14 @@ class Constant: public Distribution<T> {
 	T value;
 	public:
 		Constant(T t) { value = t; }
-		T sample() { return value; }
+		void sample(Datum<T> * d) { d->value = &value; }
 		double score(T t) { return t == value ? 1:0; }
 		
 };
 
 class Random: public Distribution<Constant<float>* > {
 	public:
-		Constant<float>* sample() { return new Constant<float>(rand()); }
+		void sample(Datum<Constant<float> > * d) { d->value = new Constant<float>(rand()); }
 		double score(Constant<float>* t) { return 1; }	
 };
 
@@ -39,24 +46,26 @@ template<class T>
 class Table;
 
 template<class T>
-class Customer {
-	T val;
+class Customer: public Datum<T> {
 	Table<T> * table;
 	public:
-		Customer(T t) { val = t; }	
+		Customer() { table = NULL; }
 };
 
 template <class T>
-class Table: public Distribution<T> {
-	Distribution<T> * dish;
+class Table: public Distribution<T>, Datum<Distribution<T> > {
+	Distribution<T> * value;
 	vector<Customer<T> > customers;
 	public:
 		Table(Distribution<T> * t) {
-			dish = t; 
 			customers = new vector<Customer<T> >(); 
 		}
-		T sample() { return dish->sample(); }
-		double score(T t) { return dish->score(t); }
+		void sample(Datum<T> * d) { 
+			if (value != NULL) {
+				value->sample(d);
+			}
+		}
+		double score(T t) { return value->score(t); }
 		void add(Customer<T> c) {
 			customers.add(c);
 		}
@@ -70,8 +79,8 @@ class Restaurant: public Distribution<T> {
 	float discount;
 	int sampleTable();
 	public:
-		T sample();
-		T sampleAndAdd();
+		T sample(); // Sample without changing state
+		void sample(Customer<T> * c); // This changes the state of the Restaurant object.  Can avoid change of state by immediately removing the result, and later down the line it would be faster to have a sample-no-add method.
 		double score(T t);
 };
 
@@ -85,28 +94,20 @@ int Restaurant<T>::sampleTable() {
 	return discreteSample(&cdf, tables.size() + 1);
 }
 
-template <class T>
-T Restaurant<T>::sample() {
-	int samp = sampleTable();
-	if (samp == 0) {
-		return base->sample()->sample();
-	} else {
-		return tables[samp-1]->sample();
-	}
-}
 
 template <class T>
-T Restaurant<T>::sampleAndAdd() {
+void Restaurant<T>::sample(Customer<T> * c) {
 	int samp = sampleTable();
 	Table<T> table;
 	if (samp == 0) {
-		table = new Table<T>(base->sample());
+		table = new Table<T>();
+		base->sample(table);
 		tables.add(table);
 	} else {
 		table = tables[samp-1];
 	}
 	T t = table.sample();
-	table.add(new Customer<T>(t));
+	table.add(*c);
 	return t;
 }
 
